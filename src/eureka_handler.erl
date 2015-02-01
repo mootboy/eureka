59953399b639b34e3ac80e36be4af2903c57f000
Linus Harling <linus.harling@gmail.com>
2015-02-01 23:16:52 +0100

    introduced mode and made id immutable.

--- src/eureka_handler.erl
+++ src/eureka_handler.erl
@@ -4,6 +4,7 @@
         , allowed_methods/2
         , content_types_accepted/2
         , content_types_provided/2
+        , resource_exists/2
         ]).
 
 -export([ from_json/2
@@ -11,6 +12,11 @@
         ]).
 
 -export([init_pin/1]).
+
+-define(LOW,    <<"off">>).
+-define(HIGH,   <<"on">>).
+-define(OUTPUT, <<"output">>).
+-define(INPUT,  <<"input">>).
 
 init(_Req, _Opts) -> {cowboy_rest, _Req, maps:new()}.
 
@@ -22,31 +28,37 @@ content_types_accepted(Req, State) ->
 content_types_provided(Req, State) ->
   {[ {{<<"application">>, <<"json">>, '*'}, return_json }], Req, State}.
 
+from_json(Req0, #{pin := {Key, Pin1}} = State) ->
+  {ok, Body, Req}  = cowboy_req:body(Req0),
+  {Ejson}          = jiffy:decode(Body),
+  {Key, Pin}       = update_pin({Key, Pin1}, maps:from_list(Ejson)),
+  {true, cowboy_req:set_resp_body(jiffy:encode(Pin), Req), State};
 from_json(Req0, State) ->
-  case cowboy_req:binding(pid, Req0) of
-    undefined -> {true, Req0, State};
-    Value ->
-      {ok, Body, Req}  = cowboy_req:body(Req0),
-      {Ejson}          = jiffy:decode(Body),
-      Pin              = binary_to_integer(Value),
-      update_pin(Pin, maps:from_list(Ejson)),
-      {true, cowboy_req:set_resp_body(jiffy:encode(read_pin(Pin)), Req), State}
+  {true, Req0, State}.
+
+resource_exists(Req, State) ->
+  case cowboy_req:binding(pid, Req) of
+    undefined -> {true, Req, State}; % collection
+    Value     -> case read_pin(binary_to_integer(Value)) of
+                   undefined -> {false, Req, State};
+                   Pin       -> {true, Req, State#{pin => Pin}}
+                 end
   end.
 
+return_json(Req, #{pin := {_, Pin}} = State) ->
+  {jiffy:encode(Pin), Req, State};
 return_json(Req, State) ->
-  case cowboy_req:binding(pid, Req) of
-    undefined -> {jiffy:encode(read_pins()), Req, State};
-    Value     -> Pid = binary_to_integer(Value),
-                 {jiffy:encode(read_pin(Pid)), Req, State}
-  end.
+  {jiffy:encode(read_pins()), Req, State}.
 
 %% FIXME: move the hell out of here
 init_pin(N) ->
   rpc:call(ale@raspberrypi, gpio, start_link, [{N,output}]),
-  update_pin(N, #{<<"state">> => <<"off">>
-                 , <<"id">> => <<"http://192.168.1.128:8080/"
-                                , (integer_to_binary(N))/binary
-                               >>}).
+  Data = #{ <<"state">> => <<"off">>
+          , <<"id">>    => <<"http://192.168.1.128:8080/"
+                            , (integer_to_binary(N))/binary
+                           >>
+          , <<"mode">>  => ?OUTPUT},
+  ets:insert(pins, {N, Data}).
 
 read_pins() ->
   case ets:tab2list(pins) of
@@ -54,15 +66,24 @@ read_pins() ->
     _            -> []
   end.
 
-update_pin(N, #{<<"state">> := <<"off">>} = Pin) ->
+update_pin({N, Pin1}, #{ <<"state">> := ?LOW
+                       , <<"mode">>  := ?OUTPUT
+                       } = Pin2) ->
   ok = rpc:call(ale@raspberrypi, gpio, write, [N, 0]),
-  ets:insert(pins, {N, Pin});
-update_pin(N, #{<<"state">> := <<"on">>} = Pin) ->
+  write_pin(N, Pin1, Pin2);
+update_pin({N, Pin1}, #{ <<"state">> := ?HIGH
+                       , <<"mode">>  := ?OUTPUT
+                       } = Pin2) ->
   ok = rpc:call(ale@raspberrypi, gpio, write, [N, 1]),
-  ets:insert(pins, {N, Pin}).
+  write_pin(N, Pin1, Pin2).
+
+write_pin(Key, Pin1, Pin2) ->
+  Pin3 = Pin2#{<<"id">> => maps:get(<<"id">>, Pin1)},
+  ets:insert(pins, {Key, Pin3}),
+  Pin3.
 
 read_pin(N) ->
   case ets:lookup(pins, N) of
-    [{N, Pin}|_] -> Pin;
-    _            -> {error, not_found}
+    [{_, _} = KeyedPin|_] -> KeyedPin;
+    _                     -> undefined
   end.
